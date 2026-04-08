44cc8ba06fb7fe9ab9bc6ce60f6e4842756c3f6f
Mr Techtroid <64828294+mrtechtroid@users.noreply.github.com>
2026-04-08 07:54:04 +0530

    v1.1.0 - Accessbility+Remove action field to prevent XSS

--- notify.js
+++ notify.js
@@ -1,5 +1,5 @@
 /*
-NotifyJS v1.0 by MTTOne (mrtechtroid)
+NotifyJS v1.1 by MTTOne (mrtechtroid)
 Released Under MIT License - github.com/mrtechtroid/notifyjs
 
 Thanks to https://codepen.io/juliepark/pen/vjMOKQ
@@ -7,7 +7,7 @@ for UI of notification toasts. Released under MIT.
 */
 
 let DATA_SOURCE = ""
-let NOTIFY_SCRIPT_VERSION = 104  // 1.0.4
+let NOTIFY_SCRIPT_VERSION = 110  // 1.1.0
 let NOTIFY_DATA = {}
 DATA_SOURCE = document.querySelector("script[_notify_js_]").getAttribute("data_source")
 
@@ -15,6 +15,8 @@ function createNotifyJSRoot(){
   let notif_root = document.createElement("ul")
   notif_root.setAttribute("id","notify_js_root")
   notif_root.setAttribute("style","z-index:1000;position:fixed;top:10px;right:10px;")
+  notif_root.setAttribute("aria-live","polite")
+  notif_root.setAttribute("role","alert")
   document.getElementsByTagName("body")[0].appendChild(notif_root)
 }
 
@@ -26,13 +28,11 @@ function createNotifyJSRoot(){
         NOTE: - (to:"-1" - Display always after from. | from:"0" - Display always before to)
         "notif_type": [success|error|warning|info]
         "notif_text": "This is a sample notification"
-        "action": [none|button|html],
-        "action_fn": "",
         "max_shown_time":5000,
     }
 */
 function parseNotifications(json){
-  if (!json.notify_version == 1){
+  if (json.notify_version !== 1){
     console.log("Invalid NotifyJS JSON Version")
     return false;
   }
@@ -66,7 +66,7 @@ function parseNotifications(json){
       }
     }else if (webCheck.type == "regex"){
       let f = `${location.protocol}//${location.host}${location.pathname}`
-      if (f.match(webCheck.identifier)){
+      if (!f.match(webCheck.identifier)){
         continue;
       }
     }else{
@@ -124,11 +124,6 @@ function addToast(webCheck) {
   let namo = ""
   let idno = Math.floor(Math.random() * 10000)
   let web_html_ = ``
-  if (webCheck.action == "button"){
-    web_html_ = `<button class="button-box" onclick = ${webCheck.action_fn}><h1>continue</h1></button>`
-  }else if (webCheck.action == "html"){
-    web_html_ = webCheck.action_fn
-  }
   
   if (webCheck.notif_type == "success") {
     namo = `<div class="success-box"><div class="dot"></div><div class="dot two"></div><div class="face"><div class="eye"></div><div class="eye right"></div><div class="mouth happy"></div></div><div class="message"><h1 class="alert">${webCheck.notif_title}</h1><p>${webCheck.notif_text}</p></div>${web_html_}<div class = "toasttimer" id = "toasttimer` + idno + `"></div><span class = "watermark">NotifyJS v`+parseInt(NOTIFY_SCRIPT_VERSION/100)+`.0.`+NOTIFY_SCRIPT_VERSION%10+`</span></div>`
